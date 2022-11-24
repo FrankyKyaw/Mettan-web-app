273a228f7b44370500eb3300b5327bc7514151ab
Franky <myothetkyaw@bennington.edu>
2022-11-24 16:14:53 -0500

    Styling the pages and some updates to the task list page

--- to_do/basic_app/views.py
+++ to_do/basic_app/views.py
@@ -51,7 +51,7 @@ def user_login(request):
         if user:
             if user.is_active:
                 login(request, user)
-                return HttpResponseRedirect(reverse('index'))
+                return HttpResponseRedirect(reverse('basic_app:task_list'))
             else:
                 return HttpResponse("Account Not Active")
         else:
@@ -62,12 +62,18 @@ def user_login(request):
 class TaskList(ListView):
     model = models.Task
     def get_context_data(self,**kwargs):
-        context  = super().get_context_data(**kwargs)
+        context = super().get_context_data(**kwargs)
         context['task_list'] = context['task_list'].filter(user=self.request.user)
+        context['count'] = context['task_list'].filter(complete=False).count()
         return context
 
 class TaskDetail(DetailView):
    model = models.Task
+
+   def get_context_data(self, **kwargs):
+       context = super().get_context_data(**kwargs)
+       context['task'] = context['task']
+       return context
 
 class TaskCreate(CreateView):
     model = models.Task
